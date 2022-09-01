8ff45ad9fff9e0f2074761499416da0c0c96cff3
Harrison <harrisonderick.65@gmail.com>
2022-09-01 19:26:28 +0100

    chore: backend folder set up

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => { console.log(`Listening on port ${PORT}`) });
