77b0354ff44708c0e32be8bb840411bb5526ba05
Oladimeji.Dev <salimanoladimeji33@gmail.com>
2022-09-06 15:08:29 +0100

    chore: done with the server setup with moongooes

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-
+const mongoose = require('mongoose')
+//const AuthRoute = require('./src/routes/auth.route');
+const CourseRoute = require('./src/routes/course.route');
 app.use(express.json());
 app.use(cors());
+app.use('/api', CourseRoute)
+app.get('/',(req,res)=>{
+    res.send('HELLO WELCOME TO THE E-LEARNING APP BACKEND')
+})
 
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => { console.log(`Listening on port ${PORT}`) });
+
+mongoose.connect('mongodb://localhost:27017/E-learning-app')
+.then(() => {
+  app.listen(PORT, () => {
+    console.log(`Database connected and server running on port: ${PORT}`);
+  })
+})
+.catch((err) => {console.log(new Error(err))})
