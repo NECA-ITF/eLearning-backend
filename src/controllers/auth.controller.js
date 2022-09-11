c1d618a042300996ea32f753b0cccb2741358676
Oladimeji.Dev <salimanoladimeji33@gmail.com>
2022-09-11 23:00:36 +0100

    added some feat

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -64,9 +64,52 @@ async function handleLogin(req, res){
     }
 
 }
+async function handleUpdateProfile(req,res){
+   
+    try {
+        const id = req.params.id;
+        const data = req.body
+        const user = await userModel.findOne({_id:id});
 
+        if(!user){
+            return res.status(400).json({
+                message: "User does not exists",
+                success: false,
+                statusCode: 409
+            });
+        }
+        const {fullName,email,phoneNumber} = data
+        const resData = await userModel.replaceOne({_id:id},
+            {
+                fullName:fullName,
+                email:email,
+                phoneNumber:phoneNumber,
+                password:user.password,
+                isAdmin:user.isAdmin
+            }
+        )
+
+        return res.status(200).json({
+            message:"Profile Updated",
+            success:true,
+            data:resData,
+            statusCode:200
+
+        });
+
+    } catch (error) {
+        return res.status(404).json({
+            message: "something went wrong",
+            success: false,
+            statusCode: 404,
+            error:error
+        });
+    }
+    
+}
 
 module.exports = { 
     handleRegister, 
-    handleLogin
+    handleLogin,
+    handleUpdateProfile
 };
