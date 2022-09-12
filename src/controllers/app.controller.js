9e4476cab4a46eb0656c572855d8f9fc7155dac9
Harrison Akunne <harrisonderick.65@gmail.com>
2022-09-12 05:50:28 -0700

    chore: fixed app controller

--- src/controllers/app.controller.js
+++ src/controllers/app.controller.js
@@ -1,9 +1,7 @@
 const path = require('path');
 
 async function httpServeStaticImages(req, res) {
-    console.log(path.join(__dirname, '..', 'assets', 'images', req.params.filename))
-    console.log(path.resolve(path.resolve(__dirname, '..', 'assets', 'images', req.params.filename)))
-    res.sendFile(path.join(__dirname, '..', 'assets', 'images', req.params.filename));
+    res.sendFile(path.resolve(__dirname, '..', 'assets', 'images', req.params.filename));
 }
 
 module.exports = {
