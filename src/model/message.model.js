a2e9762c2752ae661936bc243b548d7a54e88c4e
Harrison Akunne <harrisonderick.65@gmail.com>
2022-10-04 14:34:15 +0200

    chore: added read property to the messge schema

--- src/model/message.model.js
+++ src/model/message.model.js
@@ -17,6 +17,10 @@ const messageSchema = new Schema({
     message: {
         type: String,
         required: true
+    },
+    read: {
+        type: Boolean,
+        default: false
     }
 }, { timestamps: true });
 
